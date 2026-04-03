faf6a72eec248e3342b97f6f04cc5423b95d3d1e
jaideepmalhotragwl <jaideep.malhotra.gwl@gmail.com>
2026-04-03 14:27:56 +0530

    Delete src/components directory

--- src/components/ui/Card.jsx
+++
@@ -1,9 +0,0 @@
-import { C } from "../../constants.js";
-
-export function Card({ children, style = {} }) {
-  return (
-    <div style={{ background: C.card, borderRadius: 12, border: `1px solid ${C.border}`, ...style }}>
-      {children}
-    </div>
-  );
-}
